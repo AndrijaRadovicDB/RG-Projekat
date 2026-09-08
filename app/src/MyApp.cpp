c2b1237d501ddfe7dcaf4e0d6c4c1299df111938
AndrijaRadovicDB <ane.005.ar@gmail.com>
2026-09-09 01:09:03 +0200

    Implemented GUI.

--- app/src/MyApp.cpp
+++ app/src/MyApp.cpp
@@ -6,11 +6,15 @@
 
 #include <MainController.hpp>
 #include <spdlog/spdlog.h>
+#include <GuiController.hpp>
 
 namespace app {
     void MyApp::app_setup() {
         auto main_controller = register_controller<MainController>();
         main_controller->after(engine::core::Controller::get<engine::core::EngineControllersEnd>());
+        auto gui_controller = register_controller<GUIController>();
+        main_controller->after(engine::core::Controller::get<engine::core::EngineControllersEnd>());
+        main_controller->before(gui_controller);
 
         spdlog::info("App setup completed");
     }
