147dacbc7033e48ab9e47bd3251095d7875d8bd5
AndrijaRadovicDB <ane.005.ar@gmail.com>
2026-09-10 00:51:53 +0200

    Added lighting to model.

--- app/src/MainController.cpp
+++ app/src/MainController.cpp
@@ -4,14 +4,32 @@
 
 #include "../include/MainController.hpp"
 
+#include <GuiController.hpp>
 #include <engine/core/Controller.hpp>
+#include <engine/graphics/GraphicsController.hpp>
+#include <engine/graphics/OpenGL.hpp>
 #include <engine/platform/PlatformController.hpp>
 #include <engine/resources/ResourcesController.hpp>
 #include <spdlog/spdlog.h>
 
 namespace app {
+    class MainPlatformEventObserver : public engine::platform::PlatformEventObserver {
+    public:
+        void on_mouse_move(engine::platform::MousePosition position) override;
+    };
+
+    void MainPlatformEventObserver::on_mouse_move(engine::platform::MousePosition position) {
+        auto gui_controller = engine::core::Controller::get<GUIController>();
+        if (!gui_controller->is_enabled()) {
+            auto camera = engine::core::Controller::get<engine::graphics::GraphicsController>()->camera();
+            camera->rotate_camera(position.dx, position.dy);
+        }
+    }
+
     void MainController::initialize() {
-        spdlog::info("MainController initialized");
+        auto platform = engine::core::Controller::get<engine::platform::PlatformController>();
+        platform->register_platform_event_observer(std::make_unique<MainPlatformEventObserver>());
+        engine::graphics::OpenGL::enable_depth_testing();
     }
 
     bool MainController::loop() {
@@ -22,12 +40,83 @@ namespace app {
         return true;
     }
 
+    void MainController::update_camera() {
+        auto gui_controller = engine::core::Controller::get<GUIController>();
+        if (gui_controller->is_enabled()) {
+            return;
+        }
+
+        auto platform = engine::core::Controller::get<engine::platform::PlatformController>();
+        auto graphics = engine::core::Controller::get<engine::graphics::GraphicsController>();
+        auto camera   = graphics->camera();
+        float dt      = platform->dt();
+
+        if (platform->key(engine::platform::KeyId::KEY_W).is_down()) {
+            camera->move_camera(engine::graphics::Camera::Movement::FORWARD, dt);
+        }
+        if (platform->key(engine::platform::KeyId::KEY_S).is_down()) {
+            camera->move_camera(engine::graphics::Camera::Movement::BACKWARD, dt);
+        }
+        if (platform->key(engine::platform::KeyId::KEY_A).is_down()) {
+            camera->move_camera(engine::graphics::Camera::Movement::LEFT, dt);
+        }
+        if (platform->key(engine::platform::KeyId::KEY_D).is_down()) {
+            camera->move_camera(engine::graphics::Camera::Movement::RIGHT, dt);
+        }
+    }
+
+    void MainController::update() {
+        update_camera();
+    }
+
     void MainController::draw_fish() {
-        auto resources                  = engine::core::Controller::get<engine::resources::ResourcesController>();
-        engine::resources::Model *model = resources->model("fish");
+        auto resources                    = engine::core::Controller::get<engine::resources::ResourcesController>();
+        auto graphics                     = engine::core::Controller::get<engine::graphics::GraphicsController>();
+        engine::resources::Model *fish    = resources->model("fish");
+        engine::resources::Shader *shader = resources->shader("basic");
+
+        shader->use();
+        shader->set_mat4("projection", graphics->projection_matrix());
+        shader->set_mat4("view", graphics->camera()->view_matrix());
+        shader->set_vec3("viewPos", graphics->camera()->Position);
+
+        glm::mat4 model = glm::mat4(1.0f);
+        model           = glm::translate(model, glm::vec3(0.0f, 0.0f, -3.0f));
+        model           = glm::scale(model, glm::vec3(0.1f));
+        shader->set_mat4("model", model);
+        shader->set_vec3("objectColor", glm::vec3(1.0f, 1.0f, 1.0f));
+
+        shader->set_vec3("dirLight_direction", glm::vec3(-0.2f, -1.0f, 0.01f));
+        shader->set_vec3("dirLight_color", glm::vec3(0.3f, 0.3f, 0.3f));
+
+        shader->set_vec3("pointLight_position", glm::vec3(1.5f, 1.0f, 1.0f));
+        shader->set_vec3("pointLight_color", glm::vec3(1.0f, 1.0f, 1.0f));
+        shader->set_float("pointLight_const", 1.0f);
+        shader->set_float("pointLight_linear", 0.09f);
+        shader->set_float("pointLight_quadratic", 0.032f);
+
+        fish->draw(shader);
+    }
+
+    void MainController::draw_skybox() {
+        auto resources = engine::core::Controller::get<engine::resources::ResourcesController>();
+        auto skybox    = resources->skybox("mountain_skybox");
+        auto shader    = resources->shader("skybox");
+        auto graphics  = engine::core::Controller::get<engine::graphics::GraphicsController>();
+        graphics->draw_skybox(shader, skybox);
+    }
+
+    void MainController::begin_draw() {
+        engine::graphics::OpenGL::clear_buffers();
+    }
+
+    void MainController::end_draw() {
+        auto platform = engine::core::Controller::get<engine::platform::PlatformController>();
+        platform->swap_buffers();
     }
 
     void MainController::draw() {
         draw_fish();
+        draw_skybox();
     }
 } // app
